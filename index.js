2cb721529bc1ca16cefb20e2de8791bff4e653c4
Rafael Gilberto Rezende Lima <rgrl516@gmail.com>
2023-01-17 08:47:48 -0300

    Solved Lab + Bonus

--- index.js
+++ index.js
@@ -43,3 +43,60 @@ if (hacker1.toLowerCase() < hacker2.toLowerCase()) {
 } else {
   console.log("What?! You both have the same name?");
 }
+//Bonus 1
+let longText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`;
+let count = 0;
+for (let i = 0; i < longText.length - 1; i++) {
+  if (
+    longText[i] + longText[i + 1] + longText[i + 2] + longText[i + 3] ==
+    " et "
+  ) {
+    count++;
+  }
+}
+console.log(count);
+//Bonus 2
+let phraseToCheck = `Was it a car or a cat I saw?`;
+let phrase1 = "";
+let phrase2 = "";
+for (let i = 0; i < phraseToCheck.length; i++) {
+  if (
+    phraseToCheck[i] == " " ||
+    phraseToCheck[i] == "," ||
+    phraseToCheck[i] == "!" ||
+    phraseToCheck[i] == "?" ||
+    phraseToCheck[i] == "'" ||
+    phraseToCheck[i] == "." ||
+    phraseToCheck[i] == ":" ||
+    phraseToCheck[i] == ";" ||
+    phraseToCheck[i] == "-" ||
+    phraseToCheck[i] == `"`
+  ) {
+    continue;
+  }
+  phrase1 += phraseToCheck[i].toLowerCase();
+}
+for (let i = phraseToCheck.length - 1; i > -1; i--) {
+  if (
+    phraseToCheck[i] == " " ||
+    phraseToCheck[i] == "," ||
+    phraseToCheck[i] == "!" ||
+    phraseToCheck[i] == "?" ||
+    phraseToCheck[i] == "'" ||
+    phraseToCheck[i] == "." ||
+    phraseToCheck[i] == ":" ||
+    phraseToCheck[i] == ";" ||
+    phraseToCheck[i] == "-" ||
+    phraseToCheck[i] == `"`
+  ) {
+    continue;
+  }
+  phrase2 += phraseToCheck[i].toLowerCase();
+}
+if (phrase1 == phrase2) {
+  console.log("Palindrome!");
+} else {
+  console.log("Not Palindrome.");
+}
